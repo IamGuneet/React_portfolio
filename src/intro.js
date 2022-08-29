932f30b13230332a67b09d83de0fdab70a93e904
Guneet <guneet.iam@gmail.com>
2022-08-29 17:26:16 +0530

    Navbar added

---
+++ src/intro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function intro() {
+  return (
+    <div className='container'>
+      <div className='intro'>
+        <div className='title'>
+        <h1 className='display-1'>Guneet Singh</h1>
+        <h3>Uni Student</h3>
+        <h3>
+        ...Coder...
+        </h3>
+        </div>
+        <img src='/' alt='img'/>
+      </div>
+    </div>
+  )
+}
---
+++ src/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>
+     
+     <nav class="navbar navbar-expand-lg navbar-light display-7 ">
+  <div class="container-fluid">
+    <a class=" navbar-brand m-2 " id='name' href="#/">Guneet Singh</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Skills</a>
+        <a class="nav-link" href="#">Projects</a>
+        <a class="nav-link" href="#">Contact</a>
+        {/* <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> */}
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default navbar

f973882b07bc6c9ae58a428d9cff74804c216995
Guneet <guneet.iam@gmail.com>
2022-08-29 17:59:39 +0530

    Home added

---
+++ src/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function about() {
+  return (
+    <>
+      <div className='container'>
+     <div className='about'>
+    About
+
+     </div>
+      </div>
+      <hr/>
+    </>
+  )
+}
+
+export default about
--- src/intro.js
+++ src/intro.js
@@ -1,18 +1,21 @@
 import React from 'react'
-
+import pic from './img/me.JPG'
 export default function intro() {
   return (
+    <>
     <div className='container'>
       <div className='intro'>
         <div className='title'>
-        <h1 className='display-1'>Guneet Singh</h1>
+        <h2 className='display-1 name'>Guneet Singh</h2>
         <h3>Uni Student</h3>
         <h3>
         ...Coder...
         </h3>
         </div>
-        <img src='/' alt='img'/>
+        <img className='profile-pic' src={pic} alt='img'/>
       </div>
     </div>
+      <hr/>
+    </>
   )
 }
